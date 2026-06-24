a88b4513d5c60eabbf5ea91049264dbd5b754a96
Aman Pandey <2004amanpandey@gmail.com>
2026-06-24 09:07:59 +0530

    Day 20 self

---
+++ Day 20/Self/first.js
@@ -0,0 +1,5 @@
+// document.getElementById("header2").innerHTML="Ye hu na baat";
+//  const hem=document.getElementsByClassName("header1");
+//  hem[0].innerHTML="hello aman"
+ const list=document.querySelector("ul li");
+ console.log(list)
